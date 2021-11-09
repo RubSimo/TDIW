f6536953db242097ab715fc527748af1b77be169
ruben <rubensimomarin@gmail.com>
2021-11-09 15:23:27 +0100

    Initial commit

---
+++ formulari.js
@@ -0,0 +1,6 @@
+function confirmaRegistre(){
+    alert("confirmant registre...");
+    document.getElementById("contenidor").innerHTML = "<p class='Important'>T'has registrat amb èxit!</p>";
+    console.log("registre confirmat!");
+    return false;
+}
